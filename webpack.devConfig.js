48403d0665dba2fd45f61f22fc273bb90140a05f
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2023-01-14 18:06:58 +0300

    first webpack commit

---
+++ webpack.devConfig.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
+const __dirname = path.resolve();
+
+const config = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+  },
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
+
+export default config;
---
+++ webpack.prodConfig.js
@@ -0,0 +1,57 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __dirname = path.resolve();
+
+const config = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: './images/[name].[hash][ext][query]',
+
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new MiniCssExtractPlugin({ filename: '[name].[hash].css' }),
+  ],
+};
+
+export default config;
